d7b00bd67ca051fead2a7bacb8dec4b3c5492a42
tal itach <tgamer553@gmail.com>
2017-09-08 13:28:45 +0300

    Add project files.

---
+++ TorChanChat/TorChanChat.py
@@ -0,0 +1,111 @@
+import socket
+import socks
+import sys
+import os
+import encryptionlib
+from thread import start_new_thread
+
+class Client():
+    def __init__(self,username,addr,port):
+        self.rsa = encryptionlib.RSA()
+        self.pubk, self.prvk = self.rsa.generate_keypair()
+        self.username = username
+        os.system("tor")
+        self.members = []
+        socks.set_default_proxy("socks5","127.0.0.1",9050)
+        self.sock = socks.socksocket()
+        self.sock.connect((addr,port))
+        self.sock.send(self.username)
+        self.sock.send(self.pubk)
+        self.GetUsers()
+        start_new_thread(self.listen,())
+        start_new_thread(self.send,())
+
+    def GetUsers(self):
+        data = self.sock.recv(1024)
+        data = repr(data)
+        for users in data.split(' '):
+            self.members.append((users.split(':')[0],users.split(':')[1]))
+
+    def listen(self):
+        while 1:
+            data = self.sock.recv(1024)
+            data = repr(data)
+            if data == "pubkey?":
+                self.sock.send(self.pubk)
+                continue
+
+            print self.rsa.decrypt(self.prvk,data)
+
+    def send(self):
+        while 1:
+            message = raw_input("enter message/> ")
+            for usr in self.members:
+                self.sock.send(usr[0] + ": " + self.rsa.encrypt(usr[1],message))
+
+class utils:
+    def GetUser(username,users):
+        for usr in users:
+            if usr.username == username:
+                return usr        
+
+class user:
+    def __init__(self,username,addr,conn):
+        self.username = username
+        self.addr = addr
+        self.conn = conn   
+
+class Server:
+    def __init__(self,port):
+        self.clients = []
+        self.port = port
+
+    def broadcast(self,usr,data):            
+        for users in self.clients:            
+            users.conn.send(usr.username + ":" + data)
+
+    def send(self,usr,data):
+        usr.conn.send(data)
+
+    def handler(self,usr):
+        while 1:
+            data = usr.conn.recv(1024)
+            data = repr(data)
+            self.send(utils.GetUser(data.split(' ')[0]),data.split(' ')[1])
+
+    def sendlst(self):
+        data = ""
+        for usr in self.clients:
+            usr.conn.send("pubkey?")
+            data += usr.username + ":" + usr.conn.recv(1024)
+        return data
+
+    def Run(self):
+        sock = socket.socket()
+        sock.bind(('0.0.0.0',self.port))
+        sock.listen(5)
+        while 1:
+            conn , addr = sock.accept()
+            username = conn.recv(1024)
+            pubk = conn.recv(1024)                        
+            usr = user(username,addr[0],conn)
+            self.broadcast(usr,pubk)
+            conn.send(self.sendlst())
+            self.clients.append(usr)
+            start_new_thread(self.handler,(usr))
+
+
+
+def startup(opts):
+    if opts[1] == '-s':
+        Server(opts[2]).Run()
+    else:
+        usrname = raw_input('enter username: ')
+        Client(usrname,opts[2],opts[3])
+
+
+print "welcome to Tor Chan Chat (:"
+print "use -s port to start a server"
+print "or -c with addr and port to start as a client"
+print "good luck and have fun (: "
+startup(sys.argv)
---
+++ TorChanChat/encryptionlib.py
@@ -0,0 +1,104 @@
+import hashlib
+import os
+import sys
+import random
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
+class RSA:
+    def random_prime(self):
+        n = random.randint(10**10+1,10**11)
+        while not self.is_prime(n):
+            n = random.randint(10**10+1,10**11)
+
+    def is_prime(self,num):
+        if num == 2:
+            return True
+        if num < 2 or num % 2 == 0:
+            return False
+        for n in range(3, int(num**0.5)+2, 2):
+            if num % n == 0:
+                return False
+        return True
+
+    def __init__(self):       
+        self.q = random_prime()
+        self.p = random_prime()
+
+    def __init__(self,emp):
+        self.emp = emp
+
+    def gcd(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+    def multiplicative_inverse(e, phi):
+        d = 0
+        x1 = 0
+        x2 = 1
+        y1 = 1
+        temp_phi = phi    
+        while e > 0:
+            temp1 = temp_phi/e
+            temp2 = temp_phi - temp1 * e
+            temp_phi = e
+            e = temp2        
+            x = x2- temp1* x1
+            y = d - temp1 * y1        
+            x2 = x1
+            x1 = x
+            d = y1
+            y1 = y    
+        if temp_phi == 1:
+            return d + phi
+
+    def generate_keypair(self):
+        if self.p == self.q:
+            raise ValueError('p and q cannot be equal')
+        n = self.p * self.q
+        phi = (self.p-1) * (self.q-1)
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+        while g != 1:
+            e = random.randrange(1, phi)
+            g = gcd(e, phi)
+        d = multiplicative_inverse(e, phi)
+        #returns pub , prv
+        return ((e, n), (d, n))
+
+    def encrypt(pk, plaintext):#public
+        key, n = pk
+        cipher = [(ord(char) ** key) % n for char in plaintext]
+        return cipher
+
+    def decrypt(pk, ciphertext):#private
+        key, n = pk
+        plain = [chr((char ** key) % n) for char in ciphertext]    
+        return ''.join(plain)
+   
+class AESClass:
+
+    def __init__(self, key): 
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
